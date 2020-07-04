3a9165b5e1aea66084f5a3b809e4252208d5b356
Gauravkumamwat <gauravkumawat630@gmail.com>
2020-07-04 15:11:16 +0530

    thanks
    hiii

---
+++ ram.php
@@ -0,0 +1,77 @@
+
+<html>
+<title> BOOTS</title>
+<head>
+  <link rel="stylesheet"  type="text/css" href="stylen.css">
+   <meta charset="utf-8">
+    <link rel="stylesheet"  type="
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../../maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="../../ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="../../maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<header>
+<nav class="navbar">
+<div class="logo">
+</div>
+<div class="contest-btn">
+<a href="https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp">
+
+Google Login - Sign in</a>
+</div>
+
+<div class="menu">
+
+<ul>
+  <li><a href="https://en.wikipedia.org/wiki/Home_page" class="active">Home</a></li>
+  
+  <li><a href="https://blog.hubspot.com/service/best-contact-us-pages">Contact</a></li>
+  
+    
+  <li><a href="http://phpfiddle.org/">PHP</a></li>
+  <li><a href="https://www.dictionary.com/browse/about">About</a></li>
+    <li><a href="https://www.w3schools.com/html/default.asp">w3 school</a></li>
+</ul>
+</div>
+
+
+<div class="contest-btn">
+<a href="http://www.facebook.com/" target="_blank"> facebook </a>
+
+
+
+
+</div>
+</nav>
+<div class="center-contact">
+<h1> Hello gaurav kumawat</h1>
+
+<h2> <center>  Software Development Engineer </center></h2> 
+
+
+
+</header>
+
+  
+<div class="wrep">
+<div class="search">
+<a href="https://www.google.com/">
+
+<input type="text" class="searchterm" name="" placeholder="  Search for google ">
+<button type="submit"  class="searchbtn">
+
+<i class ="fa fa-search"></i>
+
+</button>
+</div>
+<div class ="button">
+<a href="https://www.amazon.in/gp/customer-reviews/R1HE7Y66S5MW2Y?ASIN=B003JLHU2I" class="btn">  <small>Amazon.in</small></a>  <class="btn"><small> </small></a>
+
+<a href="https://www.google.com/maps/@26.7388018,75.2829042,15z" class="btn">  <small>Maps</small></a>
+
+</div>
+</body>
+</html>
